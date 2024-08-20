fb0c8ba4c521637954fc0ae2717d3bb9275ee500
cleitonbrandao <cbs3dmax@gmail.com>
2024-08-20 16:13:25 -0300

    criação de organization

---
+++ apps/api/src/http/routes/organization/create-organization.ts
@@ -0,0 +1,71 @@
+import { auth } from '@/http/middlewares/auth';
+import { prisma } from '@/ilb/prisma';
+import { FastifyInstance } from 'fastify';
+import { ZodTypeProvider } from 'fastify-type-provider-zod';
+import { z } from 'zod';
+import { BadRequestError } from '../_erros/bad-request-error';
+import { createSlug } from '@/utils/create-slug';
+import { organizationSchema } from '@cbsaas/auth';
+
+export async function createOrganization(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .post(
+      '/organization',
+      {
+        schema: {
+          tags: ['organizaitons'],
+          summary: 'Create a New Organizaiton',
+          security: [{ bearerAuth: [] }],
+          body: z.object({
+            name: z.string(),
+            domain: z.string().nullish(),
+            shouldAttachUserByDomain: z.boolean().optional(),
+          }),
+          response: {
+            201: z.object({
+              organizationId: z.string(),
+            }),
+          },
+        },
+      },
+      async (request, reply) => {
+        const userId = await request.getCurrentUserId();
+        const { name, domain, shouldAttachUserByDomain } = request.body;
+
+        if (domain) {
+          const organizationByDomain = await prisma.organization.findUnique({
+            where: { domain },
+          });
+
+          if (organizationByDomain) {
+            throw new BadRequestError(
+              'Another organization with same domain already exist.'
+            );
+          }
+        }
+
+        const organization = await prisma.organization.create({
+          data: {
+            name,
+            slug: createSlug(name),
+            domain,
+            shouldAttachUserByDomain,
+            ownerId: userId,
+
+            members: {
+              create: {
+                userId,
+                role: 'ADMIN',
+              },
+            },
+          },
+        });
+
+        return reply.status(201).send({
+          organizationId: organization.id,
+        });
+      }
+    );
+}
--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,62 +1,73 @@
-import { fastifyCors } from '@fastify/cors'
-import {fastify} from 'fastify'
-import { fastifyJwt } from '@fastify/jwt'
-import { fastifySwagger } from '@fastify/swagger'
-import { fastifySwaggerUi } from '@fastify/swagger-ui'
+import { fastifyCors } from '@fastify/cors';
+import { fastify } from 'fastify';
+import { fastifyJwt } from '@fastify/jwt';
+import { fastifySwagger } from '@fastify/swagger';
+import { fastifySwaggerUi } from '@fastify/swagger-ui';
 import {
-    jsonSchemaTransform,
-    serializerCompiler,
-    validatorCompiler,
-    ZodTypeProvider
-} from 'fastify-type-provider-zod'
-import { createAccount } from './routes/auth/create-account'
-import { authenticateWithPassword } from './routes/auth/authenticate-with-password'
-import { getProfile } from './routes/auth/get-profile'
-import { errorHandler } from './error-handler'
-import { requestPasswordRecover } from './routes/auth/request-password-recover'
-import { resetPassword } from './routes/auth/reset-password'
-import { env } from '@cbsaas/env'
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from 'fastify-type-provider-zod';
+import { createAccount } from './routes/auth/create-account';
+import { authenticateWithPassword } from './routes/auth/authenticate-with-password';
+import { getProfile } from './routes/auth/get-profile';
+import { errorHandler } from './error-handler';
+import { requestPasswordRecover } from './routes/auth/request-password-recover';
+import { resetPassword } from './routes/auth/reset-password';
+import { env } from '@cbsaas/env';
+import { createOrganization } from './routes/organization/create-organization';
 
-const app = fastify().withTypeProvider<ZodTypeProvider>()
+const app = fastify().withTypeProvider<ZodTypeProvider>();
 
-app.setSerializerCompiler(serializerCompiler)
-app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
 
-app.setErrorHandler(errorHandler)
+app.setErrorHandler(errorHandler);
 
 app.register(fastifySwagger, {
-    openapi: {
-      info: {
-        title: 'Cbs SaaS',
-        description: 'Full-stack saas app multi-tennant & RBAC',
-        version: '1.0.0',
-      },
-      servers: [],
+  openapi: {
+    info: {
+      title: 'Cbs SaaS',
+      description: 'Full-stack saas app multi-tennant & RBAC',
+      version: '1.0.0',
     },
-    transform: jsonSchemaTransform,
-    // You can also create transform with custom skiplist of endpoints that should not be included in the specification:
-    //
-    // transform: createJsonSchemaTransform({
-    //   skipList: [ '/documentation/static/*' ]
-    // })
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+    },
+  },
+  transform: jsonSchemaTransform,
+  // You can also create transform with custom skiplist of endpoints that should not be included in the specification:
+  //
+  // transform: createJsonSchemaTransform({
+  //   skipList: [ '/documentation/static/*' ]
+  // })
 });
 
 app.register(fastifySwaggerUi, {
-    routePrefix: '/docs',
-})
+  routePrefix: '/docs',
+});
 
 app.register(fastifyJwt, {
-    secret: env.JWT_SECRET,
-})
+  secret: env.JWT_SECRET,
+});
 
-app.register(fastifyCors)
+app.register(fastifyCors);
 
-app.register(createAccount)
-app.register(authenticateWithPassword)
-app.register(getProfile)
-app.register(requestPasswordRecover)
-app.register(resetPassword)
+app.register(createAccount);
+app.register(authenticateWithPassword);
+app.register(getProfile);
+app.register(requestPasswordRecover);
+app.register(resetPassword);
 
-app.listen({port: env.SERVER_PORT}).then(() => {
-    console.log('HTTP server running!')
-})
+app.register(createOrganization);
+
+app.listen({ port: env.SERVER_PORT }).then(() => {
+  console.log('HTTP server running!');
+});
