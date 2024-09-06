05602766464884e2545ccb5500b9619e3e2f3064
cleitonbrandao <cbs3dmax@gmail.com>
2024-09-06 17:14:28 -0300

    correção e redirecionamento do organization switcher

---
+++ apps/web/src/app/(app)/org/[slug]/page.tsx
@@ -0,0 +1,9 @@
+import { Header } from '../../../../components/headers';
+export default async function Projects() {
+    return (
+        <div className="py-4">
+            <Header />
+            <main></main>
+        </div>
+    )
+}
--- apps/web/src/components/organization-switcher.tsx
+++ apps/web/src/components/organization-switcher.tsx
@@ -6,17 +6,32 @@ import { DropdownMenu,
     DropdownMenuItem,
     DropdownMenuSeparator
  } from "./ui/dropdown-menu";
-import { ChevronDown, CirclePlusIcon, Link } from 'lucide-react';
+import { ChevronDown, CirclePlusIcon } from 'lucide-react';
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { GetOrganizations } from "http/get-organizations";
+import Link from "next/link";
+import { cookies } from "next/headers";
+
 export async function OrganizationSwitcher() {
+    const currentOrg = cookies().get('org')?.value
     const { organizations } = await GetOrganizations()
+
+    const currentOrganization = organizations.find((org) => org.slug === currentOrg)
     return (
         <DropdownMenu>
             <DropdownMenuTrigger 
-            className="flex w-[168px] items-center gap-2 
-            rounded p-1 text-sm font-medium outline-none focus-visible:ring-2 focus-visible:ring-primary">
-                <span className="text-muted-foreground">Select organization</span>
+            className="flex w-[168px] items-center gap-2 rounded p-1 text-sm font-medium outline-none focus-visible:ring-2 focus-visible:ring-primary">
+                {currentOrganization ? (
+                    <>
+                        <Avatar className="mr-2 size-4">
+                            {currentOrganization.avatarUrl && <AvatarImage src={currentOrganization.avatarUrl}/>}
+                            <AvatarFallback />
+                        </Avatar>
+                        <span className="truncate text-left">{currentOrganization.name}</span>
+                    </>
+                ) : (
+                    <span className="text-muted-foreground">Select organization</span>
+                )}
                 <ChevronDown className="ml-auto size-4 text-muted-foreground"/>
             </DropdownMenuTrigger>
             <DropdownMenuContent align="end" alignOffset={-16} className="w-[200px]">
@@ -24,24 +39,24 @@ export async function OrganizationSwitcher() {
                     <DropdownMenuLabel>Organizations</DropdownMenuLabel>
                     {organizations.map(organization => {
                         return (
-                            <DropdownMenuItem key={organization.id}>
-                                <a href={`/org/${organization.slug}`}>
+                            <DropdownMenuItem key={organization.id} asChild>
+                                <Link href={`/org/${organization.slug}`}>
                                     <Avatar className="mr-2 size-4">
                                         {organization.avatarUrl && <AvatarImage src={organization.avatarUrl}/>}
                                         <AvatarFallback />
                                     </Avatar>
                                     <span className="line-clamp-1">{organization.name}</span>
-                                </a>
+                                </Link>
                         </DropdownMenuItem>
                         )
                     })}
                 </DropdownMenuGroup>
                 <DropdownMenuSeparator />
                 <DropdownMenuItem asChild>
-                    <a href="">
+                    <Link href="">
                         <CirclePlusIcon className="mr-2 size-4"/>
                         Create new
-                    </a>
+                    </Link>
                 </DropdownMenuItem>
             </DropdownMenuContent>
         </DropdownMenu>
