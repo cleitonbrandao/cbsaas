525c4cad5b72871693b66496eebc61d263af7c81
CleitonBrandao <cbs3dmax@gmail.com>
2024-10-14 20:20:05 -0300

    currency monetary

--- apps/web/src/components/Inputs/InputMonetary/Monetary.tsx
+++ apps/web/src/components/Inputs/InputMonetary/Monetary.tsx
@@ -1,13 +1,30 @@
 import { Input } from "@/components/ui/input"
-import { forwardRef, InputHTMLAttributes } from "react";
+import { forwardRef, InputHTMLAttributes, useState } from "react";
 
 type InputProps = InputHTMLAttributes<HTMLInputElement>;
 
 
 export const Monetary = forwardRef<HTMLInputElement, InputProps>(({ type="text", name="", ...props }, ref) => {
+    const [value, setValue] = useState("");
+
+    const formatNumber = (value: string) => {
+        const numericValue = value.replace(/[^\d]/g, "");
+        if (!numericValue) return "";
+        
+        return numericValue
+          .replace(/\B(?=(\d{3})+(?!\d))/g, ".")
+          .replace(/(\d+)(\d{2})$/, "$1,$2");
+      };
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const formattedValue = formatNumber(e.target.value);
+        setValue(formattedValue);
+    }
     return (
         <>
-            <Input type={type} name={name} ref={ref} {...props}/>
+            <Input type={type} name={name} ref={ref} value={value} onChange={handleChange} {...props} />
         </>
     )
 })
+
+Monetary.displayName = 'Monetary';
